43a72064784cda9ecf7489cbfbc3e76093e96941
Japalm5 <47685843+Japalm5@users.noreply.github.com>
2021-07-23 11:43:53 -0500

    Add files via upload
    Script checks Google Chromes version to ensure it meets a certain version requirement.

---
+++ version_check.ps1
@@ -0,0 +1,5 @@
+﻿$pattern = '9*.*'
+$version = (Get-ChildItem 'C:\Program Files\Google\Chrome\Application\chrome.exe').VersionInfo.ProductVersion
+Write-Host $version
+cls
+IF($Version -like $pattern){Write-Host 'Result: Pass' $version 'is supported'}ELSE {Write-Host 'Result: Fail' $version 'is not supported'}
